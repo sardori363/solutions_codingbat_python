c2bf569a3c66be80721494eb35008f9af9fc2e7a
Odiljonov Sardorbek <89182202+sardori363@users.noreply.github.com>
2023-01-16 18:23:31 +0500

    Update warmup-1.py

--- warmup-1.py
+++ warmup-1.py
@@ -53,6 +53,11 @@ sum_double(1, 2) → 3
 sum_double(3, 2) → 5
 sum_double(2, 2) → 8
 '''
+def sum_double(a, b):
+  if a==b:
+    return (a+b)*2
+  else:
+    return a+b
 
 '''
                                             DIFF21
